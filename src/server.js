5615e98c654ed7908768ab0782ec26b712cc388f
Gabriel Oliveira <g4bri3l.0lv@gmail.com>
2020-07-20 16:58:41 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ nunjucks.configure("src/views", {
 })
 
 server.get("/", (req,res) => {
-    return res.render("index.html", { title: "pspsps" })
+    return res.render("index.html", { title: "Um titulo" })
 })
 server.get("/create-point", (req,res) => {
 
